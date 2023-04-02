2affcb026395535784b423210f0d1c0124ef4871
Sumeyra Gunel <h.berengunel@gmail.com>
2023-04-02 10:53:25 +0300

    02 Nisan

--- src/Day04_Proje/Islemler.java
+++ src/Day04_Proje/Islemler.java
@@ -6,8 +6,11 @@ import java.util.Scanner;
 public class Islemler {
 
     Scanner scan = new Scanner(System.in);
+    Ogrenci ogrenci;
 
-    void menu(List<Ogrenci> ogrList){
+    int numara=1000;
+
+    void menu(List<Ogrenci> ogrList) {
         System.out.println("============= İŞLEMLER =============\n" +
                 "             1-EKLEME\n" +
                 "             2-LISTELEME\n" +
@@ -19,19 +22,40 @@ public class Islemler {
 
         char secim = scan.next().toUpperCase().charAt(0);
 
-        switch (secim){
-            case '1' :{}
-            case '2' :{}
-            case '3' :{}
-            case '4' :{}
-            case 'Q' :{}
+        switch (secim) {
+            case '1': {
+                ekleme();
+            }
+            case '2': {
+            }
+            case '3': {
+            }
+            case '4': {
+            }
+            case 'Q': {
+            }
 
 
+        }
 
+    }
+
+    private void ekleme() {
+        System.out.println("OGRENCININ ADINI GIRINIZ: ");
+        String ad = scan.nextLine();
+        System.out.println("OGRENCININ SOYADINI GIRINIZ: ");
+        String soyad = scan.nextLine();
+        System.out.println("OGRENCININ TC NO SUNU GIRINIZ:  ");
+        String tcNo = scan.next();
+        System.out.println("OGRENCININ YASINI GIRINIZ:  ");
+        int yas = scan.nextInt();
+        System.out.println("OGRENCININ SINIFINI GIRINIZ:  ");
+        int sinif = scan.nextInt();
+
+        ogrenci = new Ogrenci(ad,soyad,tcNo,yas,numara++,sinif);
 
 
 
 
     }
-
 }
